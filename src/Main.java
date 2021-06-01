c560bdfec5bd4129c539bf60f36eb7bfc14ad093
YosefSchwartz <73064850+YosefSchwartz@users.noreply.github.com>
2021-06-01 17:09:50 +0300

    Finish!

--- src/Main.java
+++ src/Main.java
@@ -32,20 +32,57 @@ public class Main {
 
     public static void main(String[] args) {
         Graph g = new Graph();
-        for(int i = 0; i<10;i++){
+        for(int i = 0; i<18;i++){
             g.addNode(new NodeData());
         }
-        g.addEdge(8,0);
         g.addEdge(0,1);
+        g.addEdge(0,7);
 
-        g.addEdge(1,2);
+        g.addEdge(1,4);
+
         g.addEdge(2,3);
-        g.addEdge(3,4);
-        g.addEdge(3,7);
-        g.addEdge(4,5);
+        g.addEdge(2,5);
+        g.addEdge(2,6);
+
+        g.addEdge(3,6);
+
+        g.addEdge(4,7);
+        g.addEdge(4,8);
+
         g.addEdge(5,6);
-        g.addEdge(6,7);
-        g.addEdge(7,9);
+        g.addEdge(5,12);
+        g.addEdge(5,13);
+
+        g.addEdge(6,13);
+
+        g.addEdge(7,8);
+        g.addEdge(7,10);
+
+        g.addEdge(8,9);
+        g.addEdge(8,10);
+
+        g.addEdge(9,12);
+        g.addEdge(9,13);
+
+        g.addEdge(10,11);
+
+        g.addEdge(11,12);
+        g.addEdge(11,14);
+        g.addEdge(11,15);
+
+        g.addEdge(12,13);
+        g.addEdge(12,16);
+        g.addEdge(12,17);
+
+        g.addEdge(13,16);
+        g.addEdge(13,17);
+
+        g.addEdge(14,15);
+
+        g.addEdge(15,16);
+
+        g.addEdge(16,17);
+
 
         System.out.println(g);
 
@@ -64,7 +101,9 @@ public class Main {
             T = new Graph();
             T.addNode(r);
             while(!BFS.isEmpty()){
-                NodeData v = g.getConvertNode(BFS.poll());
+                NodeData vOriginal = BFS.poll();
+                NodeData v = g.getConvertNode(vOriginal);
+                System.out.println("V is = "+v.getKey());
                 for(NodeData w : g.getNi(v)){
                     if(!T.get_all_V().contains(w) && w.getMatch()){
                         T.addNode(w);
